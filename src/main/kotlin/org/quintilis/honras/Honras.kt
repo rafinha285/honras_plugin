fe00fbd0152c4d1177779df683568bd9884e0520
Rafael <jcschrott10@gmail.com>
2025-01-12 03:13:59 -0300

    first commit

---
+++ src/main/kotlin/org/quintilis/honras/Honras.kt
@@ -0,0 +1,14 @@
+package org.quintilis.honras
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Honras : JavaPlugin() {
+
+    override fun onEnable() {
+        
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
